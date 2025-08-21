db483ce5bee699bee87a628331468cc68e670f41
Prose IM Homebrew Tap updater <runner@pkrvmqc4gcfdwos.zlxcakwy2jjudj4ucvtnllunvd.bx.internal.cloudapp.net>
2025-08-21 09:11:20 +0000

    feat: Prose 0.12.1

--- Casks/prose.rb
+++ Casks/prose.rb
@@ -1,12 +1,12 @@
 cask "prose" do
-  version "0.12.0"
+  version "0.12.1"
 
   arch arm: "aarch64", intel: "x86_64"
   arch_short = on_arch_conditional arm: "aarch64", intel: "x64"
   platform = "macos"
 
-  sha256 arm:   "297c979fe76f9d23d7114c283c93b2220c4fd893b22880fe9b1bec537933fc42",
-         intel: "c715b3260db18406cfeceae674331ec00ffc3bdb6a7d84167a9ce9db3e09278b"
+  sha256 arm:   "f88cdfbdcef1a860dc0c00254980341b9edb27fce5bba5c08b0eaa9f6a31c512",
+         intel: "c1b6b7e25d3a1ad46fb2edbc0b4df3ff7c4dfea81db7ea30427bfdfee6f93aeb"
 
   url "https://files.prose.org/apps/versions/#{version}/#{platform}/#{arch}/Prose_#{version}_#{arch_short}.dmg"
   name "Prose"
